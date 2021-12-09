212aae80ec62565f257bd53fb412f667527d5d83
Shivam Sapate <ssapate@dotgo.com>
2021-12-09 17:15:34 +0530

    Added Singly linked list with display operation

---
+++ src/Datastructure/LinkedList/SinglyList/SinglyLinkedList.java
@@ -0,0 +1,50 @@
+package Datastructure.LinkedList.SinglyList;
+
+
+public class SinglyLinkedList {
+	private Node head;
+
+	private static class Node {
+		int data;
+		Node next;
+
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	public void display() {
+		Node current = head;
+		while(current!=null) {
+			System.out.print(current.data+"-->");
+			current=current.next;
+		}
+		System.out.println("null");
+	}
+
+	public void lenthOfLinkedList() {
+		Node current=head;
+		int count=0;
+		while(current!=null) {
+			count++;
+			current=current.next;
+		}
+		System.out.println("The lengh of linkedList :"+count);
+	}
+	
+	public static void main(String[] args) {
+		SinglyLinkedList sl = new SinglyLinkedList();
+		sl.head = new Node(10);
+		Node second = new Node(20);
+		Node third = new Node(30);
+		Node forth = new Node(40);
+		
+		sl.head.next=second;
+		second.next=third;
+		third.next=forth;
+		
+		sl.display();
+		sl.lenthOfLinkedList();
+	}
+}
