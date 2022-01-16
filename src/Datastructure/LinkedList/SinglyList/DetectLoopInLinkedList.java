0186d536222f27dc5f5234d842ee6d5931a84ee1
shivam sapate <shivamsapate@gmail.com>
2022-01-16 21:07:57 +0530

    created package for array related data structure

---
+++ src/Datastructure/LinkedList/SinglyList/DetectLoopInLinkedList.java
@@ -0,0 +1,57 @@
+package Datastructure.LinkedList.SinglyList;
+
+public class DetectLoopInLinkedList {
+    private Node head;
+
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void display(){
+        Node curr = head;
+        while(curr!=null){
+            System.out.print(curr.data+"-> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    public int detectLoopInLinkedlist(){
+        Node slowNode = head;
+        Node FastNode = head;
+        while(FastNode!=null && FastNode.next!=null){
+            FastNode = FastNode.next.next;
+            slowNode = slowNode.next;
+            if(slowNode==FastNode)
+                return 1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        DetectLoopInLinkedList obj = new DetectLoopInLinkedList();
+    
+    // creating node objects
+        obj.head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        Node forth = new Node(40);
+    
+    // Let's connect object to each other
+        obj.head.next = second;
+        second.next = third;
+        third.next = forth;
+        forth.next = obj.head;
+
+        int bool =  obj.detectLoopInLinkedlist();
+        System.out.println(bool);
+
+
+    }
+}
---
+++ src/Datastructure/LinkedList/SinglyList/RemoveKeyFromLinkedList.java
@@ -0,0 +1,61 @@
+package Datastructure.LinkedList.SinglyList;
+
+import java.util.Scanner;
+
+public class RemoveKeyFromLinkedList {
+    private Node head;
+
+    public static class Node {
+        public int data;
+        public Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void display(){
+        Node curr = head;
+        while(curr!=null){
+            System.out.print(curr.data+"-> ");
+            curr = curr.next;
+        }
+        System.out.print("null");
+    }
+
+    public void removeKeyFromLinkedList(int n){
+        Node curr = head;
+        Node temp = null;
+        while(curr!=null && curr.data!=n){
+            temp = curr;
+            curr = curr.next;
+        }
+        if(curr.next!=null){
+            temp.next = curr.next;
+        }else
+            temp.next = null;
+    }
+
+    public static void main(String[] args) {
+        RemoveKeyFromLinkedList obj = new RemoveKeyFromLinkedList();
+        
+        //creating all nodes
+        obj.head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+        Node forth = new Node(40);
+
+        //linking all nodes to each other
+        obj.head.next = second;
+        second.next= third;
+        third.next = forth;
+
+        obj.display();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        obj.removeKeyFromLinkedList(n);
+        obj.display();
+
+    }
+}
